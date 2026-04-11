9c49143bedd13cfaeae7f8789df1afec366fba92
jojimenez78 <garciaalonso527Q@gmail.com>
2026-04-11 00:21:57 -0600

    employes

--- spa_django/spa/urls.py
+++ spa_django/spa/urls.py
@@ -4,10 +4,16 @@ from .views import (
     crear_cita,
     mis_citas,
     cancelar_cita,
+
     lista_servicios,
     crear_servicio,
     editar_servicio,
     eliminar_servicio,
+
+    lista_empleados,
+    crear_empleado,
+    editar_empleado,
+    eliminar_empleado,
 )
 
 urlpatterns = [
@@ -19,5 +25,12 @@ urlpatterns = [
     path('admin-servicios/', lista_servicios, name='lista_servicios'),
     path('admin-servicios/nuevo/', crear_servicio, name='crear_servicio'),
     path('admin-servicios/editar/<int:servicio_id>/', editar_servicio, name='editar_servicio'),
+
     path('admin-servicios/eliminar/<int:servicio_id>/', eliminar_servicio, name='eliminar_servicio'),
+    path('admin-empleados/', lista_empleados, name='lista_empleados'),
+path('admin-empleados/nuevo/', crear_empleado, name='crear_empleado'),
+path('admin-empleados/editar/<int:empleado_id>/', editar_empleado, name='editar_empleado'),
+path('admin-empleados/eliminar/<int:empleado_id>/', eliminar_empleado, name='eliminar_empleado'),
+
+    
 ]
